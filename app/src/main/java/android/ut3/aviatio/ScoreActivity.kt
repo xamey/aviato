5fe01ffeb7734995ef7c63d1afbaf69070299178
xamey <ecallemax@gmail.com>
2020-03-13 08:15:15 +0100

    init

---
+++ app/src/main/java/android/ut3/aviatio/ScoreActivity.kt
@@ -0,0 +1,12 @@
+package android.ut3.aviatio
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScoreActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_score)
+    }
+}
