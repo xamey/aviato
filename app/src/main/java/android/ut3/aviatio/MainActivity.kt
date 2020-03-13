032cd6e702fd8b200712ba9c532b56b65b9efbc9
Grégory Alary <gregalary26@gmail.com>
2020-03-13 11:01:42 +0100

    11:01

--- app/src/main/java/android/ut3/aviatio/MainActivity.kt
+++ app/src/main/java/android/ut3/aviatio/MainActivity.kt
@@ -6,27 +6,28 @@ import android.content.pm.PackageManager
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.Handler
 import android.os.Vibrator
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import be.tarsos.dsp.io.android.AudioDispatcherFactory
 import be.tarsos.dsp.onsets.OnsetHandler
 import be.tarsos.dsp.onsets.PercussionOnsetDetector
-import android.os.Build
-import android.os.VibrationEffect
-import android.hardware.SensorManager
-import android.media.MediaPlayer
-import android.widget.Toast
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class MainActivity : AppCompatActivity(), SensorEventListener {
 
     val PERMISSION_ID = 42
 
-    val SENSITIVITY = 67.5 // TODO
+    val SOUND_SENSITIVITY = 50.0 // TODO
 
     private lateinit var mSensorManager: SensorManager;
     private var mProximity: Sensor? = null;
@@ -35,6 +36,11 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     private lateinit var vibrator: Vibrator;
     private var mediaPlayer: MediaPlayer? = null;
 
+    private var timer: Timer? = null
+    private var timerTask: TimerTask? = null
+    private var timerStartTime: Long = 0
+    private val handler: Handler = Handler()
+
     private var rp: Float = -1f
     private var rl: Float =-1f;
 
@@ -42,6 +48,11 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     private val PROX_POCKET_TRESHOLD = 1f;
 
     private lateinit var stopButton: Button;
+    private lateinit var timerTv: TextView
+    private lateinit var phoneInPocketTv: TextView
+
+    private var alertIsStarted: Boolean = false;
+    private var isInThePocket: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -56,6 +67,9 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
         setUpAmbientSongListener();
         stopButton = findViewById(R.id.stopBtn);
+        timerTv = findViewById(R.id.timerTv)
+        phoneInPocketTv = findViewById(R.id.phoneInPocketTv)
+        phoneInPocketTv.visibility = View.VISIBLE
         stopButton.isEnabled = false;
         stopButton.setOnClickListener {
             stopAlert()
@@ -75,6 +89,7 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             mSensorManager.registerListener(this, mProximity, SensorManager.SENSOR_DELAY_NORMAL)
         }
         mSensorManager.registerListener(this, mLight, SensorManager.SENSOR_DELAY_NORMAL)
+        initializeTimerTask()
     }
 
     override fun onPause() {
@@ -82,15 +97,19 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         mSensorManager.unregisterListener(this)
     }
 
-    private fun vibrate() {
-        val pattern: LongArray = longArrayOf(0,200,0)
-        if (vibrator.hasVibrator()) {
-            vibrator.vibrate(pattern,0);
-        } else {
-            fallbackEmitSong();
-        }
-        runOnUiThread {
-            stopButton.isEnabled = true
+    private fun startAlert() {
+        if (!alertIsStarted && isInThePocket) {
+            val pattern: LongArray = longArrayOf(0,200,0)
+            if (vibrator.hasVibrator()) {
+                vibrator.vibrate(pattern,0);
+            } else {
+                fallbackEmitSong();
+            }
+            startTimer()
+            runOnUiThread {
+                stopButton.isEnabled = true
+            }
+            alertIsStarted = true;
         }
     }
 
@@ -102,14 +121,13 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
         val dispatcher = AudioDispatcherFactory.fromDefaultMicrophone(22050, 1024, 0)
 
         val threshold = 8.0
-        val sensitivity = SENSITIVITY
+        val sensitivity = SOUND_SENSITIVITY
 
         val mPercussionDetector = PercussionOnsetDetector(22050f, 1024,
             object : OnsetHandler {
-
                 override fun handleOnset(time: Double, salience: Double) {
                     println("Clap detected!")
-                    vibrate();
+                    startAlert();
                 }
             }, sensitivity, threshold
         )
@@ -139,10 +157,11 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
 
     private fun detect(prox: Float, light: Float) {
         if ((prox < PROX_POCKET_TRESHOLD || mProximity == null) && (light < LIGHT_POCKET_TRESHOLD)) {
-            println("In the pocket")
+            isInThePocket = true;
         } else if ((prox >= PROX_POCKET_TRESHOLD || mProximity == null) && light >= LIGHT_POCKET_TRESHOLD) {
-            println("Not in the pocket")
+            isInThePocket = false;
         }
+        phoneInPocketTv.visibility = if (isInThePocket || alertIsStarted) View.GONE else View.VISIBLE
     }
 
     private fun requestPermissions() {
@@ -157,7 +176,8 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
     }
 
     private fun stopAlert() {
-        if (stopButton.isEnabled) {
+        if (alertIsStarted && stopButton.isEnabled) {
+            stoptimertask()
             if (vibrator.hasVibrator()) {
                 vibrator.cancel();
             } else {
@@ -165,6 +185,36 @@ class MainActivity : AppCompatActivity(), SensorEventListener {
             }
             runOnUiThread {
                 stopButton.isEnabled = false
+            }
+            alertIsStarted = false;
+        }
+    }
+
+    fun startTimer() {
+        if (timer == null) {
+            timerStartTime = System.currentTimeMillis()
+            timer = Timer()
+            initializeTimerTask()
+            timer!!.schedule(timerTask, 0, 30)
+        }
+    }
+
+    fun stoptimertask() { //stop the timer, if it's not already null
+        if (timer != null) {
+            timer!!.cancel()
+            timer = null
+        }
+    }
+
+    fun initializeTimerTask() {
+        timerTask = object : TimerTask() {
+            override fun run() {
+                handler.post {
+                    var millis: Long = (System.currentTimeMillis() - timerStartTime)
+                    timerTv.setText(
+                        (SimpleDateFormat("ss:SSS")).format(Date(millis))
+                    )
+                }
             }
         }
     }
